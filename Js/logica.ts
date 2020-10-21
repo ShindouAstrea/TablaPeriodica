f8564eb1bd02a97a71fb4f7549f0666432f70713
ShindouDoushin <jose.silva.a@mail.pucv.cl>
2020-10-21 19:33:11 -0300

    lista editada

--- Js/logica.ts
+++ Js/logica.ts
@@ -5,59 +5,725 @@ interface elementos {
 }
 let listaElementos:elementos[] = [
     {
-        nombre:"",
+        nombre:"Sy",
+        creadora:"",
+        area:"Ciencias naturales",
+
+    },
+    {
+        nombre:"St",
+        creadora:"",
+        area:"Ciencias naturales",
+
+    },
+    {
+        nombre:"Lh",
+        creadora:"",
+        area:"Ciencias naturales",
+
+    },
+    {
+        nombre:"Ba",
+        creadora:"",
+        area:"Ciencias naturales",
+
+    },
+    {
+        nombre:"Mh",
+        creadora:"",
+        area:"Ciencias naturales",
+
+    },
+    {
+        nombre:"Th",
+        creadora:"",
+        area:"Ciencias naturales",
+
+    },
+    {
+        nombre:"Ca",
+        creadora:"",
+        area:"Ciencias naturales",
+
+    },
+    {
+        nombre:"La",
+        creadora:"",
+        area:"Inventoras/ingenieras",
+
+    },
+    {
+        nombre:"Ay",
+        creadora:"",
+        area:"Inventoras/ingenieras",
+
+    },
+    {
+        nombre:"Cr",
+        creadora:"",
+        area:"Inventoras/ingenieras",
+
+    },
+    {
+        nombre:"Ho",
+        creadora:"",
+        area:"Inventoras/ingenieras",
+
+    },
+    {
+        nombre:"K",
+        creadora:"",
+        area:"Inventoras/ingenieras",
+
+    },
+    {
+        nombre:"Ha",
+        creadora:"",
+        area:"Inventoras/ingenieras",
+
+    },
+    {
+        nombre:"An",
+        creadora:"",
+        area:"Paleontologas",
+
+    },
+    {
+        nombre:"Lk",
+        creadora:"",
+        area:"Paleontologa",
+
+    },
+    {
+        nombre:"Gd",
+        creadora:"",
+        area:"Primatologas",
+    },
+    {
+        nombre:"Lo",
+        creadora:"",
+        area:"Matematicas",
+
+    },
+    {
+        nombre:"C",
+        creadora:"",
+        area:"Matematicas",
+
+    },
+    {
+        nombre:"Bs",
+        creadora:"",
+        area:"Matematicas",
+
+    },
+    {
+        nombre:"Ag",
+        creadora:"",
+        area:"Matematicas",
+
+    },
+    {
+        nombre:"Gv",
+        creadora:"",
+        area:"Matematicas",
+
+    },
+    {
+        nombre:"Ge",
+        creadora:"",
+        area:"Matematicas",
+
+    },
+    {
+        nombre:"Mz",
+        creadora:"",
+        area:"Matematicas",
+
+    },
+
+    {
+        nombre:"Jh",
+        creadora:"",
+        area:"Matematicas",
+
+    },
+    {
+        nombre:"No",
+        creadora:"",
+        area:"Matematicas",
+
+    },
+    {
+        nombre:"Ko",
+        creadora:"",
+        area:"Matematicas",
+
+    },
+    {
+        nombre:"H",
+        creadora:"Matematicas",
+        area:"",
+
+    },
+    {
+        nombre:"Lp",
+        creadora:"",
+        area:"Matematicas",
+
+    },
+    {
+        nombre:"Wk",
+        creadora:"",
+        area:"Astronomas",
+
+    },
+    {
+        nombre:"Hr",
+        creadora:"",
+        area:"Astronomas",
+
+    },
+    {
+        nombre:"Mi",
+        creadora:"",
+        area:"Astronomas",
+
+    },
+    {
+        nombre:"Z",
+        creadora:"",
+        area:"Astronomas",
+
+    },
+    {
+        nombre:"Fl",
+        creadora:"",
+        area:"Astronomas",
+
+    },
+    {
+        nombre:"Lv",
+        creadora:"",
+        area:"Astronomas",
+
+    },
+    {
+        nombre:"Cn",
+        creadora:"",
+        area:"Astronomas",
+
+    },
+    {
+        nombre:"Rm",
+        creadora:"",
+        area:"Astronomas",
+
+    },
+    {
+        nombre:"Py",
+        creadora:"",
+        area:"Astronomas",
+
+    },
+    {
+        nombre:"Ru",
+        creadora:"",
+        area:"Astronomas",
+
+    },
+    {
+        nombre:"Be",
+        creadora:"",
+        area:"Astronomas",
+
+    },
+    {
+        nombre:"Ti",
+        creadora:"",
+        area:"Astronomas",
+
+    },
+    {
+        nombre:"Cu",
+        creadora:"",
+        area:"Fisicas",
+
+    },
+    {
+        nombre:"Jc",
+        creadora:"",
+        area:"Fisicas",
+
+    },
+    {
+        nombre:"Pe",
+        creadora:"",
+        area:"Fisicas",
+
+    },
+    {
+        nombre:"W",
+        creadora:"",
+        area:"Fisicas",
+
+    },
+    {
+        nombre:"Mt",
+        creadora:"",
+        area:"Fisicas",
+
+    },
+    {
+        nombre:"Gp",
+        creadora:"",
+        area:"Fisicas",
+
+    },
+    {
+        nombre:"Sr",
+        creadora:"",
+        area:"Fisicas",
+
+    },
+    {
+        nombre:"Bu",
+        creadora:"",
+        area:"Fisicas",
+
+    },
+    {
+        nombre:"Dr",
+        creadora:"",
+        area:"Fisicas",
+
+    },
+    {
+        nombre:"Jk",
+        creadora:"",
+        area:"Fisicas",
+
+    },
+    {
+        nombre:"Tk",
+        creadora:"",
+        area:"Fisicas",
+
+    },
+    {
+        nombre:"Fy",
+        creadora:"",
+        area:"Fisicas",
+
+    },
+    {
+        nombre:"Hg",
+        creadora:"",
+        area:"Bioquimica/Biologia/Medicina",
+
+    },
+    {
+        nombre:"Cw",
+        creadora:"",
+        area:"Quimicas",
+
+    },
+    {
+        nombre:"Fr",
+        creadora:"",
+        area:"Quimicas",
+
+    },
+    {
+        nombre:"Nd",
+        creadora:"",
+        area:"Quimicas",
+
+    },
+    {
+        nombre:"Pz",
+        creadora:"",
+        area:"Quimicas",
+
+    },
+    {
+        nombre:"Pc",
+        creadora:"",
+        area:"Quimicas",
+
+    },
+    {
+        nombre:"Bl",
+        creadora:"",
+        area:"Bioquimica/Biologia/Medicina",
+
+    },
+    {
+        nombre:"Sc",
+        creadora:"",
+        area:"Bioquimica/Biologia/Medicina",
+
+    },
+    {
+        nombre:"Mg",
+        creadora:"",
+        area:"Bioquimica/Biologia/Medicina",
+
+    },
+    {
+        nombre:"Rc",
+        creadora:"",
+        area:"Quimicas",
+
+    },
+    {
+        nombre:"Sm",
+        creadora:"",
+        area:"Matematicas",
+
+    },
+    {
+        nombre:"Bb",
+        creadora:"",
+        area:"Quimicas",
+
+    },
+    {
+        nombre:"Ln",
+        creadora:"",
+        area:"Quimicas",
+
+    },
+    {
+        nombre:"Yn",
+        creadora:"",
+        area:"Quimicas",
+
+    },
+
+    {
+        nombre:"Al",
+        creadora:"",
+        area:"Quimicas",
+
+    },
+    {
+        nombre:"Ap",
+        creadora:"",
+        area:"Bioquimica/Biologia/Medicina",
+
+    },
+    {
+        nombre:"T",
+        creadora:"",
+        area:"Bioquimica/Biologia/Medicina",
+
+    },
+    {
+        nombre:"Bw",
+        creadora:"",
+        area:"Bioquimica/Biologia/Medicina",
+
+    },
+    {
+        nombre:"Ng",
+        creadora:"",
+        area:"Bioquimica/Biologia/Medicina",
+
+    },
+    {
+        nombre:"Yn",
+        creadora:"",
+        area:"Bioquimica/Biologia/Medicina",
+
+    },
+    {
+        nombre:"Sv",
+        creadora:"",
+        area:"Bioquimica/Biologia/Medicina",
+
+    },
+    {
+        nombre:"Wt",
+        creadora:"",
+        area:"Bioquimica/Biologia/Medicina",
+
+    },
+    {
+        nombre:"kl",
+        creadora:"",
+        area:"Bioquimica/Biologia/Medicina",
+
+    },
+    {
+        nombre:"Ch",
+        creadora:"",
+        area:"Bioquimica/Biologia/Medicina",
+
+    },
+    {
+        nombre:"Do",
+        creadora:"",
+        area:"Bioquimica/Biologia/Medicina",
+
+    },
+    {
+        nombre:"Me",
+        creadora:"",
+        area:"Quimicas",
+
+    },
+    {
+        nombre:"Lm",
+        creadora:"",
+        area:"Bioquimica/Biologia/Medicina",
+
+    },
+    {
+        nombre:"Nu",
+        creadora:"",
+        area:"Bioquimica/Biologia/Medicina",
+
+    },
+    {
+        nombre:"Br",
+        creadora:"",
+        area:"Bioquimica/Biologia/Medicina",
+
+    },
+    {
+        nombre:"Gr",
+        creadora:"",
+        area:"Bioquimica/Biologia/Medicina",
+
+    },
+    {
+        nombre:"Y",
+        creadora:"",
+        area:"Bioquimica/Biologia/Medicina",
+
+    },
+    {
+        nombre:"Bt",
+        creadora:"",
+        area:"Bioquimica/Biologia/Medicina",
+
+    },
+    {
+        nombre:"Yw",
+        creadora:"",
+        area:"Bioquimica/Biologia/Medicina",
+
+    },
+    {
+        nombre:"Sn",
+        creadora:"",
+        area:"Bioquimica/Biologia/Medicina",
+
+    },
+    {
+        nombre:"El",
         creadora:"",
         area:"",
 
     },
     {
-        nombre:"",
+        nombre:"Co",
+        creadora:"",
+        area:"Bioquimica/Biologia/Medicina",
+
+    },
+    {
+        nombre:"Ck",
         creadora:"",
         area:"",
 
     },
     {
-        nombre:"",
+        nombre:"B",
+        creadora:"",
+        area:"Bioquimica/Biologia/Medicina",
+
+    },
+    {
+        nombre:"Ms",
         creadora:"",
         area:"",
 
     },
     {
-        nombre:"",
+        nombre:"Cb",
+        creadora:"",
+        area:"Inventoras/ingenieras",
+
+    },
+    {
+        nombre:"Rr",
+        creadora:"",
+        area:"Inventoras/ingenieras",
+
+    },
+    {
+        nombre:"Cs",
+        creadora:"",
+        area:"Matematicas",
+
+    },
+    {
+        nombre:"Cl",
+        creadora:"",
+        area:"Matematicas",
+
+    },
+    {
+        nombre:"Wo",
+        creadora:"",
+        area:"Matematicas",
+
+    },
+    {
+        nombre:"By",
+        creadora:"",
+        area:"Matematicas",
+
+    },
+    {
+        nombre:"F",
+        creadora:"",
+        area:"Astrononomas",
+
+    },
+    {
+        nombre:"Fe",
+        creadora:"",
+        area:"Astronomas",
+
+    },
+    {
+        nombre:"Ct",
+        creadora:"",
+        area:"Astronomas",
+
+    },
+    {
+        nombre:"Rd",
+        creadora:"",
+        area:"Fisicas",
+
+    },
+    {
+        nombre:"Yz",
+        creadora:"",
+        area:"Fisicas",
+
+    },
+    {
+        nombre:"Mr",
+        creadora:"",
+        area:"Fisicas",
+
+    },
+    {
+        nombre:"Mb",
+        creadora:"",
+        area:"Fisicas",
+
+    },
+    {
+        nombre:"Mb",
+        creadora:"",
+        area:"Bioquimica/Biologia/Medicina",
+
+    },
+    {
+        nombre:"Si",
+        creadora:"",
+        area:"Fisicas",
+
+    },
+    {
+        nombre:"Bn",
+        creadora:"",
+        area:"Quimicas",
+
+    },
+    {
+        nombre:"Fu",
+        creadora:"",
+        area:"Quimicas",
+
+    },
+    {
+        nombre:"Ju",
+        creadora:"",
+        area:"Quimicas",
+
+    },
+    {
+        nombre:"Mn",
+        creadora:"",
+        area:"Quimicas",
+
+    },
+    {
+        nombre:"Sl",
         creadora:"",
         area:"",
 
     },
     {
-        nombre:"",
+        nombre:"Vr",
+        creadora:"",
+        area:"Bioquimica/Biologia/Medicina",
+
+    },
+    {
+        nombre:"Bc",
         creadora:"",
         area:"",
 
     },
     {
-        nombre:"",
+        nombre:"Ni",
         creadora:"",
-        area:"",
+        area:"Bioquimica/Biologia/Medicina",
 
     },
     {
-        nombre:"",
+        nombre:"Zn",
         creadora:"",
-        area:"",
+        area:"Bioquimica/Biologia/Medicina",
 
     },
     {
-        nombre:"",
+        nombre:"Bd",
         creadora:"",
-        area:"",
+        area:" Bioquimica/Biologia/Medicina",
 
     },
     {
-        nombre:"",
+        nombre:"Mo",
         creadora:"",
-        area:"",
+        area:"Bioquimica/Biologia/Medicina",
+
+    },
+    {
+        nombre:"Av",
+        creadora:"",
+        area:"Ciencias naturales",
+
+    },
+    {
+        nombre:"Ma",
+        creadora:"",
+        area:"PaleontologaBioquimica/Biologia/Medicina",
+
+    },
+    {
+        nombre:"Cv",
+        creadora:"",
+        area:"Ciencias naturales",
 
     }
-   
 ]
 
